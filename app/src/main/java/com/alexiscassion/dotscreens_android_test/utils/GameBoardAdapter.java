d02f5d102810d34475dd9742c97cac3dbfdeab7f
Alexis Cassion <alexiscassion24@yahoo.fr>
2021-11-08 12:53:11 +0100

    cleanup

--- app/src/main/java/com/alexiscassion/dotscreens_android_test/utils/GameBoardAdapter.java
+++ app/src/main/java/com/alexiscassion/dotscreens_android_test/utils/GameBoardAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Toast;
 
 import com.alexiscassion.dotscreens_android_test.model.Player;
 import com.alexiscassion.dotscreens_android_test.view.GridCellWidget;
